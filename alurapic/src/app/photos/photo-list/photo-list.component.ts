eac03378b48cb8c0a2c4c72ef920ad0732e2474e
Gabriel Manfroi <32476134+gabemanfroi@users.noreply.github.com>
2021-03-02 21:30:54 -0300

    First commit

---
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../photo/photo';
+
+import { PhotoService } from '../photo/photo.service';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+  photos: Photo[] = [];
+  filter: string = '';
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+  }
+}
